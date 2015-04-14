fad23d9139b32a0f04acf35c8380c7f44ccfef50
leafriend <leafriend@gmail.com>
2015-04-14 22:31:31 +0900

    Pico 추가

---
+++ src/main/java/net/folab/eicic/model/Pico.java
@@ -0,0 +1,9 @@
+package net.folab.eicic.model;
+
+public class Pico extends BaseStation {
+
+    public Pico(int idx, double x, double y, double txPower) {
+        super(idx, x, y, txPower);
+    }
+
+}
