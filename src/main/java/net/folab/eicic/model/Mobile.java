ca030674dc25394d2b9cbb92a23b9cfd352fd76f
leafriend <leafriend@gmail.com>
2015-04-14 22:30:56 +0900

    Mobile 추가

---
+++ src/main/java/net/folab/eicic/model/Mobile.java
@@ -0,0 +1,23 @@
+package net.folab.eicic.model;
+
+public class Mobile {
+
+    public final int idx;
+
+    public final double x;
+
+    public final double y;
+
+    public final double qos;
+
+    Edge<Macro> macroEdge;
+
+    public Mobile(int idx, double x, double y, double qos) {
+        super();
+        this.idx = idx;
+        this.x = x;
+        this.y = y;
+        this.qos = qos;
+    }
+
+}
