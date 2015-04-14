152edd8bfbf4529e0f110ea9c2cfb7448f93b6b8
leafriend <leafriend@gmail.com>
2015-04-15 00:38:17 +0900

    Mobile의 Macro 연결 여부 지정

--- src/main/java/net/folab/eicic/algorithm/Algorithm3.java
+++ src/main/java/net/folab/eicic/algorithm/Algorithm3.java
@@ -15,6 +15,25 @@ public class Algorithm3 implements Algorithm {
     public void calculate(List<Macro> macros, List<Pico> picos,
             List<Mobile> mobiles) {
 
+        // 각 Mobile 별 Macro가 켜졌을때 Cell Association 결정
+        // 여기서는 다른 곳과 달리 Pico의 ABS여부(is_abs())를 확인하지 않고
+        // 무조건 non-ABS 값만 취한다
+        boolean[] mobileConnectsMacro = new boolean[NUM_MOBILES];
+
+        mobiles.stream().forEach(mobile -> {
+
+            double macroLambdaR = stream(mobile.getMacroLambdaR()) //
+                    .reduce(0.0, Double::sum);
+            double macroRatio = macroLambdaR / mobile.getMacro().pa3LambdaR;
+
+            double picoLambdaR = stream(mobile.getNonPicoLambdaR()) //
+                    .reduce(0.0, Double::sum);
+            double picoRatio = picoLambdaR / mobile.getPico().pa3LambdaR;
+
+            mobileConnectsMacro[mobile.idx] = macroRatio > picoRatio;
+
+        });
+
     }
 
 }
