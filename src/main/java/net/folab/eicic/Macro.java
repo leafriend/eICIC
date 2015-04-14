2eaeeeb22037ba0fd068a4377dfbbe8c0c80b598
leafriend <leafriend@gmail.com>
2015-04-14 22:09:42 +0900

    Mobile 추가

---
+++ src/main/java/net/folab/eicic/Macro.java
@@ -0,0 +1,9 @@
+package net.folab.eicic;
+
+public class Macro extends BaseStation {
+
+    public Macro(int idx, double x, double y, double txPower) {
+        super(idx, x, y, txPower);
+    }
+
+}
