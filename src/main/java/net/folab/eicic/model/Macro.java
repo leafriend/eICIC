2a9b08af4b419f796c50fc699af03ba854fd6d79
leafriend <leafriend@gmail.com>
2015-04-15 00:34:37 +0900

    pa3LambdaR 필드 추가

--- src/main/java/net/folab/eicic/model/Macro.java
+++ src/main/java/net/folab/eicic/model/Macro.java
@@ -2,6 +2,8 @@ package net.folab.eicic.model;
 
 public class Macro extends BaseStation<Macro> {
 
+    public double pa3LambdaR;
+
     public Macro(int idx, double x, double y, double txPower) {
         super(idx, x, y, txPower);
     }
--- src/main/java/net/folab/eicic/model/Pico.java
+++ src/main/java/net/folab/eicic/model/Pico.java
@@ -2,6 +2,8 @@ package net.folab.eicic.model;
 
 public class Pico extends BaseStation<Pico> {
 
+    public double pa3LambdaR;
+
     public Pico(int idx, double x, double y, double txPower) {
         super(idx, x, y, txPower);
     }
