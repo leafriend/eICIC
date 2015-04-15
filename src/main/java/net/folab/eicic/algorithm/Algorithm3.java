37076f6e70afab88a249fdaf49bf9e21b4399122
leafriend <leafriend@gmail.com>
2015-04-15 10:27:18 +0900

    Stream -> for

--- src/main/java/net/folab/eicic/algorithm/Algorithm3.java
+++ src/main/java/net/folab/eicic/algorithm/Algorithm3.java
@@ -1,6 +1,5 @@
 package net.folab.eicic.algorithm;
 
-import static java.util.Arrays.*;
 import static net.folab.eicic.Constants.*;
 import static net.folab.eicic.model.ConnectionState.*;
 
@@ -22,19 +21,21 @@ public class Algorithm3 implements Algorithm {
         // 무조건 non-ABS 값만 취한다
         final boolean[] mobileConnectsMacro = new boolean[NUM_MOBILES];
 
-        mobiles.stream().forEach(mobile -> {
+        for (Mobile mobile : mobiles) {
 
-            double macroLambdaR = stream(mobile.getMacroLambdaR()) //
-                    .reduce(0.0, Double::sum);
+            double macroLambdaR = 0.0;
+            for (int i = 0; i < mobile.getMacroLambdaR().length; i++)
+                macroLambdaR += mobile.getMacroLambdaR()[i];
             double macroRatio = macroLambdaR / mobile.getMacro().pa3LambdaR;
 
-            double picoLambdaR = stream(mobile.getNonPicoLambdaR()) //
-                    .reduce(0.0, Double::sum);
+            double picoLambdaR = 0.0;
+            for (int i = 0; i < mobile.getNonPicoLambdaR().length; i++)
+                picoLambdaR += mobile.getMacroLambdaR()[i];
             double picoRatio = picoLambdaR / mobile.getPico().pa3LambdaR;
 
             mobileConnectsMacro[mobile.idx] = macroRatio > picoRatio;
 
-        });
+        }
 
         double best_sum_lambda_r = Double.NEGATIVE_INFINITY;
         boolean[] bestMacroStates = new boolean[NUM_MACROS];
@@ -54,9 +55,8 @@ public class Algorithm3 implements Algorithm {
                 for (int i = 0; i < NUM_RB; i++) {
                     states[mobile.idx][i] = NOTHING;
                 }
-            double curr_sum_lambda_r = macros.stream().map(macro -> {
-
-                double lambdaR = 0.0;
+            double curr_sum_lambda_r = 0.0;
+            for (Macro macro : macros) {
 
                 if (macro.state) {
                     // Mobile의 Macro가 켜졌다면
@@ -66,26 +66,26 @@ public class Algorithm3 implements Algorithm {
                     int[] macro_rb_mobile = new int[NUM_RB];
                     double[] macro_rb_lambda_r = new double[NUM_RB];
 
-                    macro.forEachMobiles(mobile -> {
+                    for (Mobile mobile : macro.getMobiles()) {
 
                         if (mobileConnectsMacro[mobile.idx]) {
 
                             for (int i = 0; i < NUM_RB; i++) {
-                                double lambda_r = mobile.getMacroLambdaR()[ri];
-                                if (macro_rb_lambda_r[ri] < lambda_r) {
-                                    macro_rb_lambda_r[ri] = lambda_r;
-                                    macro_rb_mobile[ri] = mobile.idx;
+                                double lambda_r = mobile.getMacroLambdaR()[i];
+                                if (macro_rb_lambda_r[i] < lambda_r) {
+                                    macro_rb_lambda_r[i] = lambda_r;
+                                    macro_rb_mobile[i] = mobile.idx;
                                 }
-                                states[mobile.idx][ri] = NOTHING;
+                                states[mobile.idx][i] = NOTHING;
                             }
 
                         }
 
-                    });
+                    }
 
                     for (int ri = 0; ri < NUM_RB; ri++) {
                         if (macro_rb_mobile[ri] >= 0) {
-                            lambdaR += macro_rb_lambda_r[ri];
+                            curr_sum_lambda_r += macro_rb_lambda_r[ri];
                             states[macro_rb_mobile[ri]][ri] = MACRO;
                         }
 
@@ -95,35 +95,31 @@ public class Algorithm3 implements Algorithm {
                     // Mobile의 Macro가 꺼졌다면
                     // Mobile의 Pico의 ABS 여부에 따라 lambda_r 가산
 
-                    lambdaR += macro.mapMobiles(mobile -> {
-                        double macroLambdaR = 0.0;
+                    for (Mobile mobile : macro.getMobiles()) {
                         Pico pico = mobile.getPico();
                         boolean isAbs = pico.isAbs();
                         for (int ri = 0; ri < NUM_RB; ri++) {
                             if (isAbs) {
                                 if (pico.absIndexOf(ri, mobile) == 0) {
-                                    macroLambdaR += mobile.getAbsPicoLambdaR()[ri];
+                                    curr_sum_lambda_r += mobile.getAbsPicoLambdaR()[ri];
                                     states[mobile.idx][ri] = ABS_PICO;
                                 } else {
                                     states[mobile.idx][ri] = NOTHING;
                                 }
                             } else {
                                 if (pico.nonIndexOf(ri, mobile) == 0) {
-                                    macroLambdaR += mobile.getNonPicoLambdaR()[ri];
+                                    curr_sum_lambda_r += mobile.getNonPicoLambdaR()[ri];
                                     states[mobile.idx][ri] = NON_PICO;
                                 } else {
                                     states[mobile.idx][ri] = NOTHING;
                                 }
                             }
                         }
-                        return macroLambdaR;
-                    }).reduce(0.0, Double::sum);
+                    }
 
                 }
 
-                return lambdaR;
-
-            }).reduce(0.0, Double::sum);
+            }
 
             if (curr_sum_lambda_r > best_sum_lambda_r) {
                 best_sum_lambda_r = curr_sum_lambda_r;
