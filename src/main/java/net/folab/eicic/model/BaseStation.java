3f98f445352418422a311f0a0dd372e74c0751c6
leafriend <leafriend@gmail.com>
2015-04-15 10:27:04 +0900

    getMobiles()로 교체

--- src/main/java/net/folab/eicic/model/BaseStation.java
+++ src/main/java/net/folab/eicic/model/BaseStation.java
@@ -2,9 +2,6 @@ package net.folab.eicic.model;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.stream.Stream;
 
 public abstract class BaseStation<T extends BaseStation<T>> {
 
@@ -40,12 +37,8 @@ public abstract class BaseStation<T extends BaseStation<T>> {
         }
     }
 
-    public void forEachMobiles(Consumer<Mobile> action) {
-        mobiles.forEach(action);
-    }
-
-    public <R> Stream<R> mapMobiles(Function<Mobile, R> mapper) {
-        return mobiles.stream().map(mapper);
+    public List<Mobile> getMobiles() {
+        return mobiles;
     }
 
     @Override
