ce3b009fe48d612db59b667a354fc5daaa811652
leafriend <leafriend@gmail.com>
2015-04-15 16:24:28 +0900

    코드 정리
    변수명 변경 포함

--- src/main/java/net/folab/eicic/algorithm/Algorithm3.java
+++ src/main/java/net/folab/eicic/algorithm/Algorithm3.java
@@ -17,7 +17,7 @@ public class Algorithm3 implements Algorithm {
             List<Mobile> mobiles) {
 
         // 각 Mobile 별 Macro가 켜졌을때 Cell Association 결정
-        // 여기서는 다른 곳과 달리 Pico의 ABS여부(is_abs())를 확인하지 않고
+        // 여기서는 다른 곳과 달리 Pico의 ABS여부(isAbs())를 확인하지 않고
         // 무조건 non-ABS 값만 취한다
         final boolean[] mobileConnectsMacro = new boolean[NUM_MOBILES];
 
@@ -37,46 +37,46 @@ public class Algorithm3 implements Algorithm {
 
         }
 
-        double best_sum_lambda_r = Double.NEGATIVE_INFINITY;
+        double mostLambdaRSum = Double.NEGATIVE_INFINITY;
         boolean[] bestMacroStates = new boolean[NUM_MACROS];
-        ConnectionState[][] bestConnectionStates = new ConnectionState[NUM_MOBILES][NUM_RB];
+        ConnectionState[][] bestMobileStates = new ConnectionState[NUM_MOBILES][NUM_RB];
 
-        /* 가능한 모든 Macro 상태(2 ^ NUM_MACRO = 1 << NUM_MACRO)에 대한 반복문 */
-        int num_macro_state = 1 << NUM_MACROS;
-        for (int mask = 1; mask < num_macro_state; mask++) {
+        // 가능한 모든 Macro 상태(2 ^ NUM_MACRO = 1 << NUM_MACRO)에 대한 반복문
+        int macroStatesCount = 1 << NUM_MACROS;
+        for (int mask = 1; mask < macroStatesCount; mask++) {
 
-            /* Macro 상태(ON/OFF) 지정 */
+            // Macro 상태(ON/OFF) 지정
             final int _mask = mask;
             for (Macro macro : macros)
                 macro.state = 1 == (((1 << macro.idx) & _mask) >> macro.idx);
 
-            ConnectionState[][] states = new ConnectionState[NUM_MOBILES][NUM_RB];
+            ConnectionState[][] mobileStates = new ConnectionState[NUM_MOBILES][NUM_RB];
             for (Mobile mobile : mobiles)
-                for (int i = 0; i < NUM_RB; i++) {
-                    states[mobile.idx][i] = NOTHING;
-                }
-            double curr_sum_lambda_r = 0.0;
+                for (int i = 0; i < NUM_RB; i++)
+                    mobileStates[mobile.idx][i] = NOTHING;
+
+            double lambdaRSum = 0.0;
             for (Macro macro : macros) {
 
                 if (macro.state) {
                     // Mobile의 Macro가 켜졌다면
-                    // 위에서 정한 Cell Association에 따라 lambda_r 가산
+                    // 위에서 정한 Cell Association에 따라 lambdaR 가산
                     // 각 서브 채널별 할당 대상 결정
 
-                    int[] macro_rb_mobile = new int[NUM_RB];
-                    double[] macro_rb_lambda_r = new double[NUM_RB];
+                    int[] macroMobiles = new int[NUM_RB];
+                    double[] macroLambdaRs = new double[NUM_RB];
 
                     for (Mobile mobile : macro.getMobiles()) {
 
                         if (mobileConnectsMacro[mobile.idx]) {
 
                             for (int i = 0; i < NUM_RB; i++) {
-                                double lambda_r = mobile.getMacroLambdaR()[i];
-                                if (macro_rb_lambda_r[i] < lambda_r) {
-                                    macro_rb_lambda_r[i] = lambda_r;
-                                    macro_rb_mobile[i] = mobile.idx;
+                                double lambdaR = mobile.getMacroLambdaR()[i];
+                                if (macroLambdaRs[i] < lambdaR) {
+                                    macroLambdaRs[i] = lambdaR;
+                                    macroMobiles[i] = mobile.idx;
                                 }
-                                states[mobile.idx][i] = NOTHING;
+                                mobileStates[mobile.idx][i] = NOTHING;
                             }
 
                         }
@@ -84,16 +84,16 @@ public class Algorithm3 implements Algorithm {
                     }
 
                     for (int ri = 0; ri < NUM_RB; ri++) {
-                        if (macro_rb_mobile[ri] >= 0) {
-                            curr_sum_lambda_r += macro_rb_lambda_r[ri];
-                            states[macro_rb_mobile[ri]][ri] = MACRO;
+                        if (macroMobiles[ri] >= 0) {
+                            lambdaRSum += macroLambdaRs[ri];
+                            mobileStates[macroMobiles[ri]][ri] = MACRO;
                         }
 
                     }
 
                 } else {
                     // Mobile의 Macro가 꺼졌다면
-                    // Mobile의 Pico의 ABS 여부에 따라 lambda_r 가산
+                    // Mobile의 Pico의 ABS 여부에 따라 lambdaR 가산
 
                     for (Mobile mobile : macro.getMobiles()) {
                         Pico pico = mobile.getPico();
@@ -101,17 +101,17 @@ public class Algorithm3 implements Algorithm {
                         for (int ri = 0; ri < NUM_RB; ri++) {
                             if (isAbs) {
                                 if (pico.absIndexOf(ri, mobile) == 0) {
-                                    curr_sum_lambda_r += mobile.getAbsPicoLambdaR()[ri];
-                                    states[mobile.idx][ri] = ABS_PICO;
+                                    lambdaRSum += mobile.getAbsPicoLambdaR()[ri];
+                                    mobileStates[mobile.idx][ri] = ABS_PICO;
                                 } else {
-                                    states[mobile.idx][ri] = NOTHING;
+                                    mobileStates[mobile.idx][ri] = NOTHING;
                                 }
                             } else {
                                 if (pico.nonIndexOf(ri, mobile) == 0) {
-                                    curr_sum_lambda_r += mobile.getNonPicoLambdaR()[ri];
-                                    states[mobile.idx][ri] = NON_PICO;
+                                    lambdaRSum += mobile.getNonPicoLambdaR()[ri];
+                                    mobileStates[mobile.idx][ri] = NON_PICO;
                                 } else {
-                                    states[mobile.idx][ri] = NOTHING;
+                                    mobileStates[mobile.idx][ri] = NOTHING;
                                 }
                             }
                         }
@@ -121,14 +121,13 @@ public class Algorithm3 implements Algorithm {
 
             }
 
-            if (curr_sum_lambda_r > best_sum_lambda_r) {
-                best_sum_lambda_r = curr_sum_lambda_r;
+            if (lambdaRSum > mostLambdaRSum) {
+                mostLambdaRSum = lambdaRSum;
                 for (Macro macro : macros)
                     bestMacroStates[macro.idx] = macro.state;
                 for (Mobile mobile : mobiles)
-                    for (int i = 0; i < NUM_RB; i++) {
-                        bestConnectionStates[mobile.idx][i] = states[mobile.idx][i];
-                    }
+                    for (int i = 0; i < NUM_RB; i++)
+                        bestMobileStates[mobile.idx][i] = mobileStates[mobile.idx][i];
             }
 
         }
@@ -137,9 +136,8 @@ public class Algorithm3 implements Algorithm {
             macro.state = bestMacroStates[macro.idx];
 
         for (Mobile mobile : mobiles)
-            for (int i = 0; i < NUM_RB; i++) {
-                mobile.connectionStates[i] = bestConnectionStates[mobile.idx][i];
-            }
+            for (int i = 0; i < NUM_RB; i++)
+                mobile.connectionStates[i] = bestMobileStates[mobile.idx][i];
 
     }
 
