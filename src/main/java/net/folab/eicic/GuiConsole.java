3482d96edc4c35cd3d07818d3c13718c9f54c3cc
leafriend <leafriend@gmail.com>
2015-04-16 13:17:25 +0900

    GuiConsole도 Console 구현

--- src/main/java/net/folab/eicic/GuiConsole.java
+++ src/main/java/net/folab/eicic/GuiConsole.java
@@ -1,5 +1,16 @@
 package net.folab.eicic;
 
+import static java.lang.Math.*;
+import static java.lang.String.*;
+import static net.folab.eicic.Constants.*;
+
+import java.util.List;
+
+import net.folab.eicic.algorithm.Algorithm;
+import net.folab.eicic.model.Macro;
+import net.folab.eicic.model.Mobile;
+import net.folab.eicic.model.Pico;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.FormAttachment;
 import org.eclipse.swt.layout.FormData;
@@ -9,25 +20,36 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.TableItem;
 
-public class GuiConsole {
+public class GuiConsole implements Console {
 
     public static final String LAMBDA = "\u03bb";
 
     public static final String MU = "\u03bc";
 
-    public static void main(String[] args) {
+    private Algorithm algorithm;
 
-        Display display = new Display();
+    private Display display;
 
-        Shell shell = new Shell(display);
+    private Shell shell;
+
+    private Table table;
+
+    public GuiConsole(Algorithm algorithm) {
+
+        this.algorithm = algorithm;
+
+        display = new Display();
+
+        shell = new Shell(display);
         shell.setText("eICIC");
 
         Composite parent = shell;
 
-        Table table = new Table(parent, SWT.BORDER);
-        table.setLinesVisible (true);
-        table.setHeaderVisible (true);
+        table = new Table(parent, SWT.BORDER);
+        table.setLinesVisible(true);
+        table.setHeaderVisible(true);
 
         addColumn(table, "#");
         addColumn(table, "User Rate");
@@ -36,6 +58,11 @@ public class GuiConsole {
         addColumn(table, "log(Throughput)");
         addColumn(table, LAMBDA);
         addColumn(table, MU);
+
+        for (int i = 0; i < NUM_MOBILES; i++) {
+            TableItem item = new TableItem(table, SWT.NONE);
+            item.setText(0, valueOf(i));
+        }
 
         FormData layoutData = new FormData();
         layoutData.top = new FormAttachment(0, 0);
@@ -47,19 +74,46 @@ public class GuiConsole {
         FormLayout layout = new FormLayout();
         parent.setLayout(layout);
 
-        shell.open ();
-
-        while (!shell.isDisposed ()) {
-            if (!display.readAndDispatch ()) display.sleep ();
-        }
-        display.dispose ();
-
     }
 
     public static void addColumn(Table table, String text) {
         TableColumn column = new TableColumn(table, SWT.NONE);
         column.setText(text);
         column.pack();
+    }
+
+    @Override
+    public void start(Main executor) {
+        executor.execute(this, algorithm, SIMULATION_TIME);
+
+        shell.open();
+
+        while (!shell.isDisposed())
+            if (!display.readAndDispatch())
+                display.sleep();
+
+        display.dispose();
+
+    }
+
+    @Override
+    public long dump(final int t, List<Macro> macros, List<Pico> picos,
+            final List<Mobile> mobiles, long elapsed, long execute) {
+        display.asyncExec(new Runnable() {
+            @Override
+            public void run() {
+                for (Mobile mobile : mobiles) {
+                    TableItem item = table.getItem(mobile.idx);
+                    item.setText(1, format("%12.6f", mobile.getUserRate()));
+                    item.setText(2, format("%12.6f", log(mobile.getUserRate())));
+                    item.setText(3, format("%12.6f", mobile.getThroughput() / t));
+                    item.setText(4, format("%12.6f", log(mobile.getThroughput() / t)));
+                    item.setText(5, format("%12.6f", mobile.getLambda()));
+                    item.setText(6, format("%12.6f", mobile.getMu()));
+                }
+            }
+        });
+        return System.currentTimeMillis();
     }
 
 }
