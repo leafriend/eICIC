7c7652f05b390237b81b315f9d3936ac0713b664
leafriend <leafriend@gmail.com>
2015-04-17 17:50:47 +0900

    Algorithm 1/2 추가

---
+++ src/main/java/net/folab/eicic/algorithm/Algorithm1.java
@@ -0,0 +1,18 @@
+package net.folab.eicic.algorithm;
+
+import java.util.List;
+
+import net.folab.eicic.model.Macro;
+import net.folab.eicic.model.Mobile;
+import net.folab.eicic.model.Pico;
+
+public class Algorithm1 implements Algorithm {
+
+    @Override
+    public void calculate(List<Macro> macros, List<Pico> picos,
+            List<Mobile> mobiles) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+}
