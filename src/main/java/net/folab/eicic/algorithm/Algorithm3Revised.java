a0c83054f6ef14dcd864104c94d346e86834561d
leafriend <leafriend@gmail.com>
2015-06-06 16:57:05 +0900

    Implementing algorithm 3 revised -- not completed.

--- src/main/java/net/folab/eicic/algorithm/Algorithm3Revised.java
+++ src/main/java/net/folab/eicic/algorithm/Algorithm3Revised.java
@@ -1,27 +1,41 @@
 package net.folab.eicic.algorithm;
 
-import static net.folab.eicic.model.Constants.NUM_MOBILES;
-import net.folab.eicic.core.Algorithm;
-import net.folab.eicic.model.Macro;
 import net.folab.eicic.model.Mobile;
-import net.folab.eicic.model.Pico;
-import net.folab.eicic.model.StateContext;
 
-public class Algorithm3Revised implements Algorithm {
-
-    private StateContext state;
-
-    final boolean[] mobileConnectsMacro = new boolean[NUM_MOBILES];
+public class Algorithm3Revised extends Algorithm3 {
 
     @Override
-    public int getNumber() {
-        return 3;
-    }
+    public void chooseMobileConnection(Mobile[] mobiles) {
 
-    @Override
-    public StateContext calculate(int seq, Macro[] macros, Pico[] picos,
-            Mobile[] mobiles) {
-        return StateContext.getStateContext(0, macros, picos, mobiles);
+        for (Mobile mobile : mobiles) {
+
+            double macroChannel = mobile.getMacroChannel();
+            double picoChannel = mobile.getPicoChannel();
+
+            if (macroChannel > picoChannel) {
+                // Macro로 할당받은 R이 더 큰 경우
+                mobileConnectsMacro[mobile.idx] = true;
+
+            } else if (macroChannel < picoChannel) {
+                // Pico로 할당받은 R이 더 큰 경우
+                mobileConnectsMacro[mobile.idx] = false;
+
+            } else {
+
+                if (macroChannel != 0 && picoChannel != 0) {
+                    // Macro와 Pico로 할당받은 R이 같은 경우 (0은 아님)
+                    // : 항상 Pico로 연결
+                    mobileConnectsMacro[mobile.idx] = false;
+
+                } else {
+                    // FIXME Macro와 Pico로 부터 하나도 할당받지 못한 경우 (0인 경우)
+
+                }
+
+            }
+
+        }
+
     }
 
 }
