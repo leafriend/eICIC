7dc51ede88a7f548b38eaa40948a15832aa4f77b
leafriend <leafriend@gmail.com>
2015-06-08 03:27:02 +0900

    잘못 커밋된 부분 삭제

--- src/main/java/net/folab/eicic/algorithm/Algorithm2.java
+++ src/main/java/net/folab/eicic/algorithm/Algorithm2.java
@@ -4,10 +4,6 @@ import static net.folab.eicic.model.Constants.NUM_MACROS;
 import static net.folab.eicic.model.Constants.NUM_MOBILES;
 import static net.folab.eicic.model.Constants.NUM_RB;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -90,43 +86,8 @@ public class Algorithm2 implements Algorithm {
 
         assert bestMacroState >= 0;
 
-        dumpFile(seq, macros, picos, mobiles);
-
         return state;
 
-    }
-
-    private void dumpFile(int seq, Macro[] macros, Pico[] picos, Mobile[] mobiles) {
-        try (PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(
-                    "logs/Algorithm2-" + seq + ".csv", false)));) {
-
-            out.print("#seq,");
-            out.print(seq);
-            out.print("\n");
-
-            out.print("#");
-            out.print(",");
-            out.print("Activ.");
-            out.print(",");
-            out.print("R");
-            out.print(",");
-            out.print("λR");
-            out.print("\n");
-
-            for (Macro macro : macros) {
-                out.print(macro.idx);
-                out.print(",");
-                out.print(state.getMacrosIsOn()[macro.idx] ? "ON" : "OFF");
-                out.print(",");
-                out.print(String.format("%.6f", macro.getChannel()));
-                out.print(",");
-                out.print(String.format("%.6f", macro.getLambdaR()));
-                out.print("\n");
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
 }
